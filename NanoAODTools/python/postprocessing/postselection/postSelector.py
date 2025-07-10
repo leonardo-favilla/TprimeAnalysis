bbd55e90e831877a8b1576874e228c93dd5c4352
acagnotta <antimo.cagnotta@cern.ch>
2025-07-10 14:47:54 +0200

    first commit

--- NanoAODTools/python/postprocessing/postselection/postSelector.py
+++ NanoAODTools/python/postprocessing/postselection/postSelector.py
@@ -14,7 +14,6 @@ sys.path.append('../')
 username = str(os.environ.get('USER'))
 inituser = str(os.environ.get('USER')[0])
 uid      = int(os.getuid())
-# workdir  = "user" if "user" in os.environ.get('PWD') else "work"
 WorkDir  = os.environ["PWD"]
 
 usage                   = 'python3 postSelector.py -d <datasets> --dict_samples_file <dict_samples_file> --hist_folder <hist_folder> --nfiles_max <nfiles_max>'
