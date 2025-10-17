a48433898f6ddeac3827bb688acd050b2af904ce
leonardo-favilla <leonardo.favilla@cern.ch>
2025-10-17 15:36:41 +0200

    cleaning condor submission

--- NanoAODTools/python/postprocessing/postselection_xTopSF/condor_preprocess/QCD_HT1200to1500_2022/runner.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-cd /afs/cern.ch/user/l/lfavilla/
-source analysis_TPrime.sh
-cd python/postprocessing/postselection_xTopSF/
-python3 preprocess_ntuples.py -c QCD_HT1200to1500_2022 --dict_samples_file ../samples/dict_samples_2022.json --year 2022 --nfiles_max 1 --certpath $1
