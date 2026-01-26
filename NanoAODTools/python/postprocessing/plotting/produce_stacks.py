bad0d0757b5251f3fb5cc424ed3555863f77a08a
leonardo-favilla <leonardo.favilla@cern.ch>
2026-01-26 12:00:43 +0100

    added TW sample in stack producing

--- NanoAODTools/python/postprocessing/plotting/produce_stacks.py
+++ NanoAODTools/python/postprocessing/plotting/produce_stacks.py
@@ -198,14 +198,14 @@ MT_T_xbins          = array.array('d', [500, 600, 700, 800, 1000, 1400, 2000])
 PuppiMET_pt_xbins   = array.array('d', [250, 300, 350, 400, 450, 500, 600, 850])
 
 
-# for v in vars:
+for v in vars:
 # for v in [var for var in vars if var._name == "MT_T"]:
-for v in [var for var in vars if var._name == "PuppiMET_T1_pt_nominal"]:
+# for v in [var for var in vars if var._name == "PuppiMET_T1_pt_nominal"]:
 # for v in [var for var in vars if var._name in ["LeadingFatJetPt_msoftdrop", "FatJet_msoftdrop_nominal"]]:
 # for v in [var for var in vars if var._name in ["MT_T", "PuppiMET_T1_pt_nominal"]]:
-    # for r in regions.keys():
+    for r in regions.keys():
     # for r in ["SRTop"]:
-    for r in ["AH"]:
+    # for r in ["AH"]:
         ###############################################
         ############ PreProcess Histograms ############
         ############ normalization to Lumi ############
@@ -289,7 +289,7 @@ for v in [var for var in vars if var._name == "PuppiMET_T1_pt_nominal"]:
                 tmp.Scale(lumi)
             else:
                 continue
-            print(f"Background {s.label} has {tmp.GetEntries()} entries after scaling")
+            # print(f"Background {s.label} has {tmp.GetEntries()} entries after scaling")
             # print('s.label.split("_")[0]:', s.label.split("_")[0])
             # print('s.process.split("_")[0]:', s.process.split("_")[0])
             # leg_label                       = labels_dict[s.label.split("_")[0]]
