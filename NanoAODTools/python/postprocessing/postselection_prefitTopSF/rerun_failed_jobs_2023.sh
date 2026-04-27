afc7a45eb8f872d8539627a375497ce35e105b7c
leonardo-favilla <leonardo.favilla@cern.ch>
2026-04-27 10:33:22 +0200

    plotted "noVetoElectron" selection

--- NanoAODTools/python/postprocessing/postselection_prefitTopSF/rerun_failed_jobs_2023.sh
+++ NanoAODTools/python/postprocessing/postselection_prefitTopSF/rerun_failed_jobs_2023.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
 
-python3 postSelector_submitter.py -d TT_semilep_2023 --syst --dryrun
-condor_submit ./condor_syst/TT_semilep_2023_syst/condor.sub
-echo resubmitting job for TT_semilep_2023_syst
-
-python3 postSelector_submitter.py -d TT_hadr_2023 --syst --dryrun
-condor_submit ./condor_syst/TT_hadr_2023_syst/condor.sub
-echo resubmitting job for TT_hadr_2023_syst
-
-python3 postSelector_submitter.py -d ZJetsToNuNu_2jets_PT40to100_2J_2023 --syst --dryrun
-condor_submit ./condor_syst/ZJetsToNuNu_2jets_PT40to100_2J_2023_syst/condor.sub
-echo resubmitting job for ZJetsToNuNu_2jets_PT40to100_2J_2023_syst
-
